c08192bd53161147dfacabd8a0766bce2e2cc7ca
Rakesh Jha <29140@corp.hexaware.com>
2017-07-07 14:52:01 +0530

    pizza intial code

--- pizz.js
+++ pizz.js
@@ -58,6 +58,8 @@ function getWelcomeResponse(callback) {
 
 
 function getPizza(intent, session, callback){
+    let intentOderid = intent.slots;
+    console.log(intentOderid);
     const sessionAttributes = {};
     const cardTitle = 'Hello';
     const speechOutput = 'Welcome to dominoz pizz.';
